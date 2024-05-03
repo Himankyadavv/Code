89870848269547c1f0d818685fc1b30f7648fb7b
Himank yadav <129055592+Himankyadavv@users.noreply.github.com>
2024-05-03 14:18:30 +0530

    Add files via upload

---
+++ leetcodesortedarray.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> nums1 = {1,4,6,0,0,0};
+    vector<int> nums2 = {2,3,7};
+    int m = nums1.size();
+    int n = nums2.size();
+
+    int i = m-1;
+    int j = n-1;
+    int k = m+n-1;
+
+   cout<< nums1[m-1];
+   cout<<nums1[m+n-1];
+
+    
+}
